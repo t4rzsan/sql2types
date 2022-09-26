5775abb51415e0024020ace2d28068e159942f56
Jakob Christensen <jakob.christensen@hey.com>
2022-09-26 21:10:12 +0200

    Initial code

---
+++ Program.fs
@@ -0,0 +1,17 @@
+﻿open Microsoft.Data.SqlClient
+open System.Data
+
+let getSqlSchema connectionString (tableName: string) =
+    use cn = new SqlConnection(connectionString)
+    use cmd = new SqlCommand($"SELECT * FROM {tableName}", cn)
+
+    cn.Open()
+    use reader = cmd.ExecuteReader(CommandBehavior.SchemaOnly)
+    let schemaColumns = reader.GetColumnSchema()
+    schemaColumns
+
+let schemaColumns = getSqlSchema "Integrated Security=SSPI;Initial Catalog=FSharpForActuaries;Data Source=RODI\SQLEXPRESS;Trust Server Certificate=True;" "TestTable"
+
+for column in schemaColumns do
+    printfn $"{column.ColumnName}, {column.ColumnSize}, {column.DataTypeName}, {column.DataType}, {column.AllowDBNull}"
+
